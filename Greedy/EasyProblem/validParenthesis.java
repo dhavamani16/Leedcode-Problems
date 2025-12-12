f31d59abc01f5ec0542b432ba72086a51ad217f5
dhavamani16 <dhavamani17092005@gmail.com>
2025-12-12 20:46:46 +0530

    feat(gready) :add validparenthesis problem

---
+++ Greedy/EasyProblem/validParenthesis.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int minOpen = 0;
+        int maxOpen = 0;
+        for(Character ch : s.toCharArray()){
+            if(ch=='(')
+            {
+                minOpen++;
+                maxOpen++;
+            }
+            else if(ch==')')
+            {
+                maxOpen--;
+                minOpen--;
+            }
+            else 
+            {
+                minOpen--;
+                maxOpen++;
+            }
+            if(maxOpen<0)
+                return false;
+            if(minOpen<0)
+                minOpen=0;
+        }
+        return minOpen==0;
+        
+    }
+}
