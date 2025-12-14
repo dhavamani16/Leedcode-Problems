b7cbd1f055c68587cc8d0b4b2234249405d7e0d8
dhavamani16 <dhavamani17092005@gmail.com>
2025-12-14 23:35:07 +0530

    Add Jump Problem

---
+++ Greedy/MediumProblem/jumpGames.java
@@ -0,0 +1,18 @@
+package Greedy.MediumProblem;
+public class jumpGames {
+    canJump(int[] nums) {
+        
+}
+
+public boolean canJump(int[] nums) {
+        int maxIndex=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>maxIndex){
+                return false;
+            }
+            maxIndex=Math.max(maxIndex,i+nums[i]);
+        }
+        return true;
+            }
+}
+
