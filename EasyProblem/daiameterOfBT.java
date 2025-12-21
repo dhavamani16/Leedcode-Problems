70b863cd949831c1e83407453bc7f1c698e07a3c
dhavamani16 <dhavamani17092005@gmail.com>
2025-12-21 23:09:20 +0530

    fix(tree): correct diameter calculation in daiameterOfBT.java

---
+++ EasyProblem/daiameterOfBT.java
@@ -0,0 +1,20 @@
+package EasyProblem;
+
+import javax.swing.tree.TreeNode;
+
+public class daiameterOfBT {
+    int maxi=Integer.MIN_VALUE;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return maxi;
+    }
+    public int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left=height(root.left);
+        int right=height(root.right);
+        maxi=Math.max(maxi,(left+right));
+        return Math.max(left,right)+1;
+    }
+}
