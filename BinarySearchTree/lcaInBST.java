5393d0f4fa8d40fba94b823db75805d81c5c88b0
dhavamani16 <dhavamani17092005@gmail.com>
2025-12-28 19:08:32 +0530

    Add LCA binary search tree

---
+++ BinarySearchTree/lcaInBST.java
@@ -0,0 +1,17 @@
+package BinarySearchTree;
+
+import javax.swing.tree.TreeNode;
+
+public class lcaInBST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(root!=null){
+            if(p.val>root.val && q.val>root.val)
+                root=root.right;
+            else if(p.val<root.val && q.val<root.val)
+                root=root.left;
+            else
+                return root;
+        }
+        return root;
+    }
+}
